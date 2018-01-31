8520f1d28892d38ab346aad50a124337e1dd229c
AnnaMut <butorovaanna@yandex.ru>
2018-02-01 00:10:59 +0300

    Task 2 result

--- js/stat.js
+++ js/stat.js
@@ -13,7 +13,7 @@ var BAR_WIDTH = 40;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}; /** само облако, откуда взяли х и у не улавливаю**/
+}; /** само облако**/
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -26,18 +26,6 @@ var getMaxElement = function (arr) {
   return maxElement;
 }; /** функция нахождения максимального элемента **/
 
-var getColorBar = function (names) {
-  var colorBar;
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      colorBar = 'rgba(255, 0, 0, 1)';
-    } else {
-      colorBar = 'rgba(0, 0, 255, 2)';
-    }
-    return colorBar;
-  }
-}; /** функция возращающая цвет мой и других игроков что-то не работает видимо как-то не так пишу рандом Math.random() - поставила число, чтобы пропустил тревис**/
-
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -48,25 +36,16 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, (CLOUD_Y + GAP) * 2);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, (CLOUD_Y + GAP) * 3);
 
-
   for (var i = 0; i < names.length; i++) {
     ctx.fontSlyle = FONT_GAP;
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime));
-  }
 
-  for (i = 0; i < names.length; i++) {
-    ctx.fillStyle = getColorBar(names[i]);
+    var colorBar = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random() + ')'; /** не получается добавить сюда умножить на 0.8 и прибавить 0.2 **/
+    ctx.fillStyle = colorBar;
     ctx.fillRect(CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime), BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
-  }
 
-  for (i = 0; i < names.length; i++) {
-    ctx.fontSlyle = FONT_GAP;
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_HEIGHT - GAP);
   }
 };
-/** ctx.beginPath();
-    ctx.moveTo((CLOUD_X + GAP_BAR + BAR_WIDTH) + i, CLOUD_HEIGHT - CLOUD_X * i);
-    ctx.lineWidth = BAR_WIDTH;
-    ctx.lineTo((CLOUD_X + GAP_BAR + BAR_WIDTH) + i, (BAR_HEIGHT * times[i]) / maxTime * i);
-    ctx.strokeStyle = getColorBar(names[i]);
-    ctx.stroke(); вариант с линией вместо прямоугольника хотела обсудить**/
+
