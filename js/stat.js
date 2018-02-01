e9d0b1a5fa2768b3436e335a2296a13eb934aa2f
AnnaMut <butorovaanna@yandex.ru>
2018-02-01 16:40:23 +0300

    Add constant and correct color constant

--- js/stat.js
+++ js/stat.js
@@ -14,7 +14,9 @@ var GAP_BAR = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var SELF_BAR_COLOR = 'rgba(255, 0, 0, 1)';
-var PLAYERS_BAR_COLOR = 'rgba(0, 0, 255, ' + (Math.random() * 0.8 + 0.2) + ')';
+var BLUE_BAR_1 = 'rgba(0, 0, 255, ';
+var BLUE_BAR_2 = ')';
+var NAMES_ARR_YOU = 'Вы';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -46,7 +48,7 @@ var renderStatistics = function (ctx, names, times) {
     ctx.fontSlyle = FONT_GAP;
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime));
 
-    var colorBar = (names[i] === 'Вы') ? SELF_BAR_COLOR : PLAYERS_BAR_COLOR;
+    var colorBar = (names[i] === NAMES_ARR_YOU) ? SELF_BAR_COLOR : BLUE_BAR_1 + (Math.random() * 0.8 + 0.2) + BLUE_BAR_2;
     ctx.fillStyle = colorBar;
     ctx.fillRect(CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime), BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
 
